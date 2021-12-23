7701e0bf60be08ee13be8fc9725795502aac6d54
Silviane Bezerra <silvianeraquel@gmail.com>
2021-12-23 15:12:01 -0300

    Estilizando o projeto

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import Botao from './components/Botao';
+import  './index.css';
+import './estilo.css';
 
 class App extends Component {
   constructor(props){
       super(props);
       this.state = {};
+      
 
   }
 
 
   render(){
     return(
-      <div>
-        <img  src={require('./assets/biscoito.png')} />
+      <div className='container'>
+        <img  src={require('./assets/biscoito.png')} className='img' />
         <Botao />
+        <h3 className='textoFrase'> Frase número 1 aleatória...</h3>
       </div>
     );
   }
