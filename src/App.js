dcea85dd9d4c8d4e244949049a66b8dfd07bc82e
Silviane Bezerra <silvianeraquel@gmail.com>
2021-12-23 14:45:40 -0300

    Acrescentando a imagem e o componente Botao

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
+import React, { Component } from 'react';
+import Botao from './components/Botao';
+
+class App extends Component {
+  constructor(props){
+      super(props);
+      this.state = {};
+
+  }
 
 
+  render(){
+    return(
+      <div>
+        <img  src={require('./assets/biscoito.png')} />
+        <Botao />
+      </div>
+    );
+  }
+
+}
 
 export default App;
---
+++ src/components/Botao/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Botao extends Component {
+    render(){
+        return(
+            <div>
+                <button> Abrir Biscoito </button>
+            </div>
+        )
+    }
+}
+
+export default Botao;
